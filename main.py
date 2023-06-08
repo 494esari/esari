6544014a87e3420a7dd21a5c7c6a21742f223ebb
494esari <esari123@icloud.com>
2023-06-08 11:40:09 +0300

    halo

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("This line will be printed.") 
+# Tuodaan FastAPI luokka fastapi paketista
+from fastapi import FastAPI
+
+# Luodaan uusi fastapi instanssi app nimiseen muuttujaan.
+# Muuttujan nimellä on merkitystä sillä uvicorn tarvitsee tämän 
+# nimen palvelinta käynnistäessä, tässä tapauksessa se on main:app
+app = FastAPI()
+
+# Käytetään fastapi:n @app.get dekoraattoria todos endpointin luomiseen.
+# Decorator funktio suoritetaan aina ennen sen alapuolella olevaa funktiota
+# Decorator välittää sen alapuolelle määritetylle funktiolle argumentteja. 
+# Tässä tapauksessa turvaudutaan FastAPI:n dokumentaatioon jotta tiedetään mitä
+# argumentteja dekoraattorin alapuolella oleva 
+# funktio ottaa vastaan missäkin tapauksessa.
+@app.get('/todos')
+def getTodos():
+    return "Tässä palautetaan myöhemmin todo-lista"
